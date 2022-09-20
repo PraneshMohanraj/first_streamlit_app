2e98b9eb263b9ff2b628eb2856aac4e3447034d9
PraneshMohanraj <100198715+PraneshMohanraj@users.noreply.github.com>
2022-09-20 14:40:55 +0530

    Update streamlit_app.py
    Menu Items for page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("Diner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 &Blueberry Oat Meal")
+streamlit.text("Kale ,Spinach ,Rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
