d727a2a6e96be8533fdc93c6a5ebc67d096e898f
PraneshMohanraj <100198715+PraneshMohanraj@users.noreply.github.com>
2022-10-04 15:24:30 +0530

    Update streamlit_app.py
    typo correction

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.errors import URLError
+from urllib.error import URLError
 
 streamlit.title("Diner")
 streamlit.header("Breakfast Favorites")
