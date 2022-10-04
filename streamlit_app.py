9a25c2acf4216ca77e690c56e06970b169e0bd37
PraneshMohanraj <100198715+PraneshMohanraj@users.noreply.github.com>
2022-10-04 15:23:38 +0530

    Update streamlit_app.py
    code clean up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.errors import URLError
+
 streamlit.title("Diner")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣Omega 3 &Blueberry Oat Meal")
@@ -24,8 +28,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 
@@ -34,7 +36,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output as table of normalized data
 streamlit.dataframe(fruityvice_normalized)
 
-import snowflake.connector
+streamlit.stop()
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -50,3 +53,5 @@ streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')");
