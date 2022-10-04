d21c127128746f97811b6cbd7c69438e383e9af2
PraneshMohanraj <100198715+PraneshMohanraj@users.noreply.github.com>
2022-10-04 13:24:09 +0530

    Update streamlit_app.py
    removed sample response data

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ streamlit.dataframe(fruits_to_show)
 #new section for api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
 
 # normalizing data from json format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
